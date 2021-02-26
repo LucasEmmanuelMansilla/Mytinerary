82a4186f042be29133287933ffa14b0a40e49018
unknown <lucas_e_93@hotmail.com>
2021-02-26 09:40:12 -0300

    asda

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.json())
 
 app.use('/api', router)
 
-app.listen(4000, () => console.log("App listening on port 4000"))
+
+const port = proccess.env.PORT
+const host = process.env.HOST || '0.0.0.0'
+
+app.listen(port, host, () => console.log("App listening on port 4000"))
 
 
